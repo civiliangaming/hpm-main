12e48e9c9efa314f03cb9d21ed714c1102c357d7
civiliangaming <137924203+civiliangaming@users.noreply.github.com>
2024-07-03 08:07:36 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-HPM studios
+
 export default function Home() {
   return (
     <div className="container">
@@ -9,7 +9,7 @@ export default function Home() {
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-HPM studios
+
       <main>
         <Header title="Welcome." />
         <p className="description">
